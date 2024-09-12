48f4e6cdb2b8057a34be30f233b5669a64690cbc
Stephan Wende <stephan.wende@web.de>
2024-09-12 08:50:11 +0200

    navbar and header added

---
+++ src/routes/nav.jsx
@@ -0,0 +1,44 @@
+import { Component } from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import "../../scss/style.scss";
+
+
+export default class Nav extends Component {  
+
+    constructor(props) {
+        super(props);
+        console.log("Nav component instantiated");
+        console.log(props);
+
+    }
+    static propTypes = {
+        linkMap: PropTypes.object,
+    }
+
+    render() {
+        let navMap = this.props.linkMap;
+        let links = [];
+        for (const [key, [name, path]] of Object.entries(navMap)) {
+            console.log(key, path);
+                links.push(
+                    <li className="nav-item pe-2 ps-2 " key={key}>
+                        <Link className="nav-link active text-center fw-bold" to={path}>{name}</Link>
+                    </li>
+                    
+                );
+
+        }
+
+
+        return (
+
+                <nav className="d-flex justify-content-center navbar navbar-expand-sm position-relative bg-blue-100">
+                    <ul className="navbar-nav">
+                        {links}               
+                    </ul>        
+                </nav>
+
+        );
+    }
+}
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,11 +1,34 @@
 import "../../scss/style.scss";
+import Nav from "./nav";
 export default function Root() {
+
+    const nav = {
+        home: ['home', '/'],
+        spende: ['spenden', 'spenden'],
+        about: ["über uns", "about"],
+    }
+
+    
+
     return (
         <div className="d-flex flex-column h-100">
 
-                <header className="align-items-start shadow border-1 border-bottom border-blue-400 bg-blue-300">
-                    <h1 className="text-light text-center pb-5 pt-5 fw-light">Title</h1>
+                <header className="shadow border-1 border-bottom border-blue-200 bg-blue-400">
+                    <div className="d-flex flex-row position-relative">
+                        <div className="d-flex col justify-content-start">
+                            <img src="../../logo/logo1.svg" width="80px" height="80px" alt="logo" />  
+                        </div>
+                        <div className="d-flex col align-items-center justify-content-center"> 
+                        
+                            <h1 className="display-4 text-center text-light fw-light"><em>KleiderHelden</em></h1>
+                            <h3 className="text-center"><em></em></h3>
+                        </div>
+                        
+                        <div className="col">
 
+                        </div>
+                    </div>
+                    <Nav linkMap={nav} />
                 </header>
 
                 <main className="h-100 m-2 ms-5 me-5">
@@ -13,7 +36,7 @@ export default function Root() {
                 </main>
 
                 <footer className="align-items-end text-light text-center shadow border-1 border-top border-blue-400 bg-blue-300 p-3">
-                Impressum Datenschutzerklärung
+                    Impressum Datenschutzerklärung
                 </footer>
 
 
