c3a17a2cbf84f18f4955d3bee74fe4cb07db235b
Stephan Wende <stephan.wende@web.de>
2024-09-14 21:12:58 +0200

    working on the forms

---
+++ src/forms/person.jsx
@@ -0,0 +1,16 @@
+import { Component } from "react";
+import { Form } from "react-bootstrap";
+
+Component
+export default class Person extends Component{
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <Form>
+            </Form>
+        );
+    }
+}
