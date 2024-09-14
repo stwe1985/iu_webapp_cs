4b468829ec9bb8de9f7e1d46e73b12e3e8cafa31
Stephan Wende <stephan.wende@web.de>
2024-09-14 20:50:18 +0200

    adjustments on errorpage

--- src/error.jsx
+++ src/error.jsx
@@ -1,18 +1,25 @@
 import { useRouteError } from "react-router-dom";
-import { Alert } from "react-bootstrap";
+import { Alert, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
 
 export default function ErrorPage() {
     const error = useRouteError();
     console.log(error);
 
     return (
+        
         <div className="container-sm">
-            <Alert variant="danger m-5">
+            <Alert variant="danger" className=" d-flex flex-column align-items-center align-bottom">
                 <h3>Oops!</h3>
                 <p>
                     Da hat sich ein Spendenpaket verirrt..
                 </p>
-                <p>{error.message}</p>
+                <i>Status: ({error.statusText || error.message})</i><br />
+                <Link to="/">
+                    <Button variant="secondary" className="m-5 justify-content-center">Zur Hauptseite</Button>
+                </Link>
+
             </Alert>
         </div>
     );
