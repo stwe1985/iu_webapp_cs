a8e4e8b4fa564e191793bb4d670fd2b1e44af81d
Stephan Wende <stephan.wende@web.de>
2024-09-15 12:37:09 +0200

    person form updated

--- src/forms/person.jsx
+++ src/forms/person.jsx
@@ -1,6 +1,6 @@
 import "../../scss/style.scss";
 import { useState } from "react";
-import { Form, InputGroup } from "react-bootstrap";
+import { Form, InputGroup, Row, Col, FloatingLabel, Container } from "react-bootstrap";
 
 
 export default function Person() {
@@ -21,13 +21,51 @@ export default function Person() {
         }
 
         return (
-                <Form noValidate validated={validated} onChange={handleSubmit}>
-                    <InputGroup className="mb-3">
-                        <InputGroup.Text id="basic-addon1">Vorname</InputGroup.Text>
-                        <Form.Control required placeholder="Vorname" aria-label="Vorname" aria-describedby="basic-addon1"/>
-                    </InputGroup>
+            <section className="m-0">
+                <Form noValidate validated={validated} onSubmit={handleSubmit}>
+                    <Container>
+                        <Row className="gx-1">
+                            <Col md className="gy-1">   
 
+                                <FloatingLabel label="Vorname">
+                                    <Form.Control required size="lg" aria-label="Vorname" aria-describedby="s_name" placeholder="Vorname"/>
+                                </FloatingLabel>
+                            </Col>
+                            <Col md className="gy-1">
+                                <FloatingLabel label="Nachname">                            
+                                    <Form.Control required size="lg" aria-label="Nachname" aria-describedby="l_name" placeholder="Nachname"/>
+                                </FloatingLabel>
+                            </Col>
+
+                        </Row>
+                        <Row className="gx-1">
+                            <Col md="8" className="gy-1">
+                                <FloatingLabel label="Straße">
+                                    <Form.Control required size="lg" aria-label="Straße" aria-describedby="straße" placeholder="Straße"/>
+                                </FloatingLabel>
+                            </Col>
+                            <Col xs="6" md="2" className="gy-1">
+                                <FloatingLabel label="H-Nr.">
+                                    <Form.Control required size="lg" aria-label="Nr." aria-describedby="hnr" placeholder="H-Nr."/>
+                                </FloatingLabel>
+                            </Col>
+                            <Col xs="6" md="2" className="gy-1">
+                                <FloatingLabel label="PLZ">
+                                    <Form.Control required size="lg" aria-label="PLZ" aria-describedby="plz" placeholder="PLZ"/>
+                                </FloatingLabel>
+                            </Col>
+                        </Row>
+                        <Row className="gx-1">
+                            <Col xs md lg className="gy-1">
+                                <InputGroup>
+                                    <InputGroup.Text>@</InputGroup.Text>
+                                    <Form.Control required size="lg" aria-label="email" aria-describedby="email-label"/>
+                                </InputGroup>
+                            </Col>
+                        </Row>
+                    </Container>
                 </Form>
+            </section>
 
         );
 
